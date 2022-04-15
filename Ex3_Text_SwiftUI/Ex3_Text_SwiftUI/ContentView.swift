df9187de797111a4a99dfea7ec3cdd287ade7f97
SongYunGeun <rjawoakstp@naver.com>
2022-04-15 15:58:01 +0900

    swiftui text

---
+++ Ex3_Text_SwiftUI/Ex3_Text_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ex3_Text_SwiftUI
+//
+//  Created by 송윤근 on 2022/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ex3_Text_SwiftUI/Ex3_Text_SwiftUI/Ex3_Text_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex3_Text_SwiftUIApp.swift
+//  Ex3_Text_SwiftUI
+//
+//  Created by 송윤근 on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct Ex3_Text_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
