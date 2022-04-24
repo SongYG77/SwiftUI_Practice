45b91d87a330fe9f1bc2334307d6dd0ecf605798
SongYunGeun <rjawoakstp@naver.com>
2022-04-24 16:34:57 +0900

    Stepper 연습 SwiftUI

---
+++ Ex10_Stepper_SwiftUI/Ex10_Stepper_SwiftUI/Ex10_Stepper_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex10_Stepper_SwiftUIApp.swift
+//  Ex10_Stepper_SwiftUI
+//
+//  Created by 송윤근 on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Ex10_Stepper_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
